1d3d5771c073e5b3a18549faf6e0e1c045d02389
majki09 <majki0258@gmail.com>
2024-12-31 12:13:02 +0100

    - keeloq.py: inverting to hex bug fixed.

--- keeloq.py
+++ keeloq.py
@@ -4,7 +4,7 @@ class KeeloqKey:
         self.is_valid = self.validate()
         self.second_part = int(self.key[8:], 16)
         self.inverted_binary = self.invert_bits()
-        self.fixed = self.inverted_to_hex()
+        self.fixed = self.inverted_to_hex(self.inverted_binary)
         self.button = self.fixed[2]
         self.serial_number = self.fixed[3:].upper()
 
@@ -15,5 +15,5 @@ class KeeloqKey:
         binary = bin(self.second_part)[2:].zfill(32)
         return binary[::-1]
 
-    def inverted_to_hex(self):
-        return hex(int(self.inverted_binary, 2))
+    def inverted_to_hex(self, inverted):
+        return f"{int(inverted, 2):#010x}"
